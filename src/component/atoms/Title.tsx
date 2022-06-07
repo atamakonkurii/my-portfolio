bda6e5d589839ac20f37bf5590c7b4daaca4e006
atamakonkurii <morisudesu@gmail.com>
2022-06-07 21:38:21 +0900

    storybookにtailwindのスタイルが当たるようにする

--- src/component/atoms/Title.tsx
+++ src/component/atoms/Title.tsx
@@ -4,5 +4,5 @@ type Props = {
 
 export const Title = (props: Props) => {
   const { title } = props;
-  return <h1 className="pb-6 text-xl text-white md:text-3xl">{title}</h1>;
+  return <h1 className="pb-6 text-xl text-red-800 md:text-3xl">{title}</h1>;
 };
