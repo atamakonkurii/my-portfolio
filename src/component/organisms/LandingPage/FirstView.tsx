cd20996894fd157e9e363cb993f31129e7534a1e
atamakonkurii <morisudesu@gmail.com>
2022-06-09 21:16:40 +0900

    縦の長さを変更

--- src/component/organisms/LandingPage/FirstView.tsx
+++ src/component/organisms/LandingPage/FirstView.tsx
@@ -5,7 +5,7 @@ import { FirstViewSmallText } from "src/component/atoms/Text";
 
 export const FirstView: FC = () => {
   return (
-    <div className="pb-8 bg-gradient-to-r from-sky-500 to-indigo-500 ">
+    <div className="pb-16 bg-gradient-to-r from-sky-500 to-indigo-500 ">
       {/* TODO:画像がバックグラウンドで設定できるようにする */}
       {/* <div className="pb-8 bg-[url('/img/ramen.jpg')]"></div> */}
       <div className="pt-8 text-center">
