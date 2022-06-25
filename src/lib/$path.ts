fb3909a70862f8fe6559e8145ef76ff6fd33deec
atamakonkurii <morisudesu@gmail.com>
2022-06-25 21:51:05 +0900

    google formをお問合せボタンに実装

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -13,6 +13,8 @@ export type PagesPath = typeof pagesPath
 export const staticPath = {
   favicon_ico: '/favicon.ico',
   img: {
+    jamstack_stripe_logo_png: '/img/jamstack_stripe_logo.png',
+    my_image_jpg: '/img/my_image.jpg',
     ojigi_animal_inu_png: '/img/ojigi_animal_inu.png',
     ramen_jpg: '/img/ramen.jpg'
   }
