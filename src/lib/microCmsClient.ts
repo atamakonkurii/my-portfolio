1bb0683656af9e10ce5db804140e75d4b3ebb002
atamakonkurii <morisudesu@gmail.com>
2022-06-28 23:52:33 +0900

    一旦コミット

---
+++ src/lib/microCmsClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from "microcms-js-sdk";
+
+export const microCmsClient = createClient({
+  serviceDomain: "ogotte-kudasai",
+  apiKey: process.env.CMS_API_KEY!,
+});
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,7 @@
+import type { GetStaticProps } from "next";
 import type { FC } from "react";
 import { LandingPage } from "src/component/templates/LandingPage";
+import { microCmsClient } from "src/lib/microCmsClient";
 
 export const Index: FC = () => {
   return (
@@ -7,4 +9,16 @@ export const Index: FC = () => {
       <LandingPage />
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await microCmsClient.get({
+    endpoint: "news",
+    contentId: "fqu2y_4ut7zv",
+  });
+  return {
+    props: {
+      news: data.id,
+    },
+  };
 };
