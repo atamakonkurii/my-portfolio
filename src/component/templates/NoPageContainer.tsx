eba2ccc9d309dff1175cc14f3086b73f0ba31df1
atamakonkurii <morisudesu@gmail.com>
2022-07-23 13:56:57 +0900

    nopageにホームに戻るボタンが入らなくなったので修正

--- src/component/templates/NoPageContainer.tsx
+++ src/component/templates/NoPageContainer.tsx
@@ -1,22 +1,15 @@
-import Link from "next/link";
-
 export const NoPageContainer = () => {
   return (
-    <div className="pt-16">
-      <div className="flex justify-center text-3xl font-bold">
+    <div className="pt-16 pb-4">
+      <div className="flex justify-center text-xl font-bold sm:text-3xl">
         このページは作成中です。
       </div>
       <img
         src={"/img/ojigi_animal_inu.png"}
         className="mx-auto"
         alt="inu"
-        width={300}
+        width={200}
       />
-      <Link href="/">
-        <a className="flex justify-center pt-4 pb-8 text-xl font-bold text-blue-500 hover:opacity-50">
-          ホームに戻る
-        </a>
-      </Link>
     </div>
   );
 };
