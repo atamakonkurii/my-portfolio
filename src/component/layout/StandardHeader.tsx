50c64e5552eb49022660ed3ef82731d60e78904b
atamakonkurii <morisudesu@gmail.com>
2022-07-23 15:14:18 +0900

    ヘッダーの色と大きさを変更

--- src/component/layout/StandardHeader.tsx
+++ src/component/layout/StandardHeader.tsx
@@ -5,10 +5,10 @@ import { SiteLogo } from "src/component/atoms/Svg/SiteLogo";
 export const StandardHeader: FC = () => {
   return (
     <header className="fixed top-0 left-0 bg-white/0">
-      <div className="flex items-center ml-8 w-screen h-12">
+      <div className="flex items-center ml-8 w-screen h-16">
         <Link href="/">
           <a>
-            <SiteLogo color={"rgb(14 165 233)"} height={30} width={54} />
+            <SiteLogo color={"rgb(99 102 241)"} height={40} width={70} />
           </a>
         </Link>
       </div>
